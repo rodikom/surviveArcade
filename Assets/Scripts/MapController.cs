1d3bfc720293206390389804ffa48d8bb2ef60ab
Родіон Лучко <kn221_lrv@student.ztu.edu.ua>
2024-04-24 09:51:52 +0300

    Виконано генерацію мапи і додано спрайт кістки

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -27,15 +27,38 @@ public class MapController : MonoBehaviour
         RandomFillMap();
     }
 
+    private void Update()
+    {
+        RandomFillMap();
+    }
+
     private void RandomFillMap()
     {
-        for (int x = 0; x < width; x++)
+        Vector3Int playerCell = grid.WorldToCell(playerPos.position);
+
+        int radiusSquared = 20 * 20; // Квадрат радіусу кола
+
+        for (int x = playerCell.x - 20; x <= playerCell.x + 20; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = playerCell.y - 20; y <= playerCell.y + 20; y++)
             {
-                int i = Mathf.FloorToInt(Random.Range(0f, 6f));
-                tilemap.SetTile(new Vector3Int(x, y, 0) + startPos, tiles[i]);
+                Vector3Int currentCell = new Vector3Int(x, y, 0);
+                int distanceSquared = (playerCell - currentCell).sqrMagnitude;
+
+                if (distanceSquared < radiusSquared) // Перевіряємо, чи клітина знаходиться всередині кола
+                {
+                    if (!tilemap.HasTile(currentCell)) // Перевіряємо, чи в цій клітині вже є тайл
+                    {
+                        int i = Mathf.FloorToInt(Random.Range(0f, 6f));
+                        tilemap.SetTile(currentCell, tiles[i]);
+                    }
+                }
+                else // Якщо клітина знаходиться поза колом, видаляємо тайл
+                {
+                    tilemap.SetTile(currentCell, null);
+                }
             }
-        }  
+        }
     }
+
 }
